4aeca1752b12684a696952739c615ba3d6514696
njuhi <morphynjuh@mail.com>
2019-02-11 22:26:52 +0300

    save changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
 var divition = function(number1, number2) {
   return number1 / number2;
 };
@@ -21,4 +13,20 @@ var multiplication = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = multiplication(number1, number2);
+alert(result);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+var subtraction = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtraction(number1, number2);
 alert(result);
