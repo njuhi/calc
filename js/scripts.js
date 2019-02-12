2dc889522de08241054fd428645d2d20cdee8015
njuhi <morphynjuh@mail.com>
2019-02-12 16:46:00 +0300

    save changes

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,19 @@ alert(result);
 var saySomething = function(whatToSay) {
 alert(whatToSay);
 };
+if (temperature < 15) {
+  console.log("Don't forget a jacket!");
+} else if (temperature < 25) {
+  console.log("Have a nice day :)");
+} else {
+  console.log("Keep cool today!");
+}
+if (temperature < 0) {
+  console.log("Brr! Don't freeze out there!");
+} else if (temperature < 15) {
+  console.log("Don't forget a jacket!");
+} else if (temperature < 25) {
+  console.log("Have a nice day :)");
+} else {
+  console.log("Keep cool today!");
+}
